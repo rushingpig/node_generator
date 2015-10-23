aea5d004e0c2455aa467366a5c35ccea9c7f358b
rushingpig <rushingpig@163.com>
2015-10-23 16:24:02 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @des    : the main file of the "node_generator"
+ * <ul>
+ *      <li>这里选择用handlebars作为模板引擎</li>
+ *      <li>可以根据自己的实际情况和爱好选择用"jade"或者"ejs"</li>
+ * </ul>
+ * @author : pigo.can
+ * @date   : 15/10/22 下午9:41
+ * @email  : rushingpig@163.com
+ * @version: v1.0
+ */
+"use strict";
